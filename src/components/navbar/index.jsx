b3eca96cc7d9d84446f19afaf4605ba6300aa475
stevenmettler <stevenmettler4@gmail.com>
2020-09-07 11:11:45 -0400

    testing to attribute instead of href to fix

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../index.css";
-import { Container, Navbar, Dropdown, DropdownButton, Breadcrumb, BreadcrumbItem} from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Dropdown,
+  DropdownButton,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
@@ -15,20 +22,20 @@ export default function NavBar() {
             drop="right"
             title={` steven mettler `}
           >
-            <Dropdown.Item href ="/" eventKey="1">
-                home
+            <Dropdown.Item to="/" eventKey="1">
+              home
             </Dropdown.Item>
-            <Dropdown.Item href ="/about" eventKey="2">
-                about
+            <Dropdown.Item to="/about" eventKey="2">
+              about
             </Dropdown.Item>
             <Dropdown.Item href="/resume" eventKey="3">
-                resume
+              resume
             </Dropdown.Item>
             <Dropdown.Item href="/projects" eventKey="4">
-                projects
+              projects
             </Dropdown.Item>
             <Dropdown.Item href="john" eventKey="4">
-                john
+              john
             </Dropdown.Item>
           </DropdownButton>
         </Navbar>
